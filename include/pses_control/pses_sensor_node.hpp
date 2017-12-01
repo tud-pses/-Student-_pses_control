8b7376599f115354af6f9dc5b635198d2e83bc23
Josef Kinold <josef.kinold@stud.tu-darmstadt.de>
2017-12-01 17:09:14 +0100

    kompilierbaren code erstellt, Fehler in den Callbackfunktionen behoben

--- include/pses_control/pses_sensor_node.hpp
+++ include/pses_control/pses_sensor_node.hpp
@@ -4,9 +4,11 @@
 #include <std_msgs/String.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Range.h>
-#include <std_msgs/Int16.h>
+#include <std_msgs/UInt8.h>
+#include <std_msgs/Float64.h>
 #include <stdlib.h>
 #include <math.h>
+#include <signal.h>
 
 class PsesSensor {
 
@@ -16,18 +18,25 @@ public:
     // Variables
     ros::NodeHandle nh;	// use this for global access
 
+    //Functions
+    void calculateVelocity();
+
+
 private:
     // Variables
     std_msgs::UInt8 msg_hall_counter;
     std_msgs::Float64 msg_hall_dt, msg_hall_dt8;
+    float velocity;
     // Subscriber
     ros::Subscriber sub_hall_counter;
     ros::Subscriber sub_hall_dt;
     ros::Subscriber sub_hall_dt8;
 
     // Functions
-    void hall_cntCallback(std_msgs::UInt8 hall_cnt_msg, std_msgs::UInt8* m_hall_cnt);
-    void calculateVelocity();
+  static void hall_cnt_Callback(std_msgs::UInt8::ConstPtr hall_cnt_msg, std_msgs::UInt8* m_hall_cnt);
+  static void hall_dt_Callback(std_msgs::Float64::ConstPtr hall_dt_msg, std_msgs::Float64* msg_hall_dt);
+  static void hall_dt8_Callback(std_msgs::Float64::ConstPtr hall_dt8_msg, std_msgs::Float64* msg_hall_dt8);
+
 
 
 };
--- src/pses_sensor_node.cpp
+++ src/pses_sensor_node.cpp
@@ -1,33 +1,32 @@
-#include </home/pses/catkin_ws/src/pses_control/include/pses_control/pses_sensor_node.hpp>
+#include "pses_control/pses_sensor_node.hpp"
 #define _USE_MATH_DEFINES
-#define WHEEL_RADIUS 5
+#define WHEEL_RADIUS 6.5
 
 
 
 PsesSensor::PsesSensor() {          //Constructor
     sub_hall_counter= nh.subscribe<std_msgs::UInt8>("/uc_bridge/hall_cnt", 10, boost::bind(hall_cnt_Callback, _1, &msg_hall_counter));
-    sub_hall_dt     = nh.subscribe<std_msgs::UInt8>("/uc_bridge/hall_dt", 10, boost::bind(hall_dt_Callback, _1, &msg_hall_dt));
-    sub_hall_dt8    = nh.subscribe<std_msgs::UInt8>("/uc_bridge/hall_dt8", 10, boost::bind(hall_dt8_Callback, _1, &msg_hall_dt8));
-
+    sub_hall_dt     = nh.subscribe<std_msgs::Float64>("/uc_bridge/hall_dt", 10, boost::bind(hall_dt_Callback, _1, &msg_hall_dt));
+    sub_hall_dt8    = nh.subscribe<std_msgs::Float64>("/uc_bridge/hall_dt8", 10, boost::bind(hall_dt8_Callback, _1, &msg_hall_dt8));
 }
 
 //Callback function for Hall_Counter
 //writes msg from Hall Coutner Topic to local Variable
-void PsesControl::hall_cnt_Callback(std_msgs::UInt8 hall_cnt_msg, std_msgs::UInt8* m_hall_cnt){
+void PsesSensor::hall_cnt_Callback(std_msgs::UInt8::ConstPtr hall_cnt_msg, std_msgs::UInt8* m_hall_cnt){
     *m_hall_cnt = *hall_cnt_msg;
 }
-void PsesControl::hall_dt_Callback(std_msgs::Float64 hall_dt_msg, std_msgs::Float64* msg_hall_dt){
+void PsesSensor::hall_dt_Callback(std_msgs::Float64::ConstPtr hall_dt_msg, std_msgs::Float64* msg_hall_dt){
     *msg_hall_dt = *hall_dt_msg;
 }
-void PsesControl::hall_dt8_Callback(std_msgs::Float64 hall_dt8_msg, std_msgs::Float64* msg_hall_dt8){
+void PsesSensor::hall_dt8_Callback(std_msgs::Float64::ConstPtr hall_dt8_msg, std_msgs::Float64* msg_hall_dt8){
     *msg_hall_dt8 = *hall_dt8_msg;
 }
 
 //Outputs the Sensor Values and calculates the velocity
-void PSESControl::calculateVelocity(){
-    ROS_INFO("Hal Sensor: Counter: %i, dt: %f, dt8: %f", msg_hall_counter, msg_hall_dt, msg_hall_dt8);
-    float velocity = msg_hall_dt8 * WHEEL_RADIUS*M_PI/4;
-    ROSINFO("Velocity: %f", velocity);
+void PsesSensor::calculateVelocity(){
+    ROS_INFO("Counter: %i, dt: %f, dt8: %f", msg_hall_counter.data, msg_hall_dt.data, msg_hall_dt8.data);
+    velocity = msg_hall_dt.data * WHEEL_RADIUS * M_PI / 4;
+    ROS_INFO("Velocity: %f", velocity);
 }
 
 
@@ -35,10 +34,9 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "pses_sensor_node", ros::init_options::NoSigintHandler);
     ros::NodeHandle _nh("~");	// use this for private params
     ros::Rate loop_rate(10);
-
     PsesSensor sensornode;
     while (ros::ok()) {
-        calculateVelocity();
+        sensornode.calculateVelocity();
         ros::spinOnce();
         loop_rate.sleep();
     }
