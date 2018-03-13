070ccc1a3341142ce1a41ec7cb69091854eeaed7
Josef Kinold <josef.kinold@stud.tu-darmstadt.de>
2018-03-13 15:49:45 +0100

    kommentare eingefuegt

--- include/pses_control/pses_sensor_node.hpp
+++ include/pses_control/pses_sensor_node.hpp
@@ -14,74 +14,125 @@
 #include <math.h>
 #include <signal.h>
 
+//describes the distance, which the car is moved within one tick of the wheel encoders
 #define DRIVEN_DISTANCE_PER_TICK 0.0251327412 // RAD_PER_TICK * WHEEL_RADIUS
-#define WHEEL_RADIUS 6.5
 
 class pses_sensor {
 
 public:
-    pses_sensor();       //Konstruktor der Klasse
+    /* NAME: pses_sensor
+     * DESCRIPTION: constructor of the class pses_sensor
+     *              initializes the subscribers and advertisers to ros topics
+     *              set variables to default values and create parameter
+     * INPUT: -
+     * OUTPUT: -
+     */
+    pses_sensor();
 
     // Variables
-    ros::NodeHandle nh;	// use this for global access
+    ros::NodeHandle nh; //node handle of the pses_sensor_node
 
     //Functions
+    /* NAME: calculateVelocity
+     * DESCRIPTION: calculates the actual velocity using the wheel Encoder, if the motor speed is unequal zero
+     *              calculates the driven distance of the car, since the last start and the average speed over the last 5 seconds
+     *              publishes the average_speed every 5 seconds
+     * INPUT: -
+     * OUTPUT: -
+     */
     void calculateVelocity();
-    void reset();
-    void set_powertrain();
-    void publish_sensor_data();
+
+    /* NAME: publishSensorData
+     * DESCRIPTION: calls the publish function of the advertisers for usr, wheel_odom and driven distance
+     * INPUT: -
+     * OUTPUT: -
+     */
+    void publishSensorData();
 
 
 private:
   // Variables
-    std_msgs::UInt8 msg_hall_counter;
-    std_msgs::Float64 msg_hall_dt, msg_hall_dt8, velocity, driven_distance, avg_speed;
-    sensor_msgs::Range m_usr, m_usl, m_usf, m_usr_old, m_usl_old, m_usf_old;
-    float tau;
-    int data_count;
-    std_msgs::Int16 target_speed, target_steering_angle;
-    std_msgs::Int16 msg_motor_speed;
-    dynamic_reconfigure::Server<pses_control::sensorConfig> server;
-    geometry_msgs::TwistWithCovarianceStamped wheel_odom;
+    std_msgs::UInt8 msg_hall_counter; //number of ticks of the hall sensor
+    std_msgs::Float64 msg_hall_dt, msg_hall_dt8; //time between one and eight ticks of the hall sensor
+    std_msgs::Float64 velocity, driven_distance, avg_speed; //calculated velocity, driven distance and average speed of the car
+    sensor_msgs::Range m_usr; // new value of the ultra sonic sensor right
+    float usr_tau; //cutoff frequency for ultra sonic sensor right lowpass filter
+    int data_count; //counter to calculate the average speed over time
+    std_msgs::Int16 msg_motor_speed; //contains control value for motor speed
+    dynamic_reconfigure::Server<pses_control::sensorConfig> server; //dynamic parameter server
+    geometry_msgs::TwistWithCovarianceStamped wheel_odom; //contains velocity of the car
 
 
   // Subscriber
-    //hall sensor
-    ros::Subscriber sub_hall_counter;
-    ros::Subscriber sub_hall_dt;
-    ros::Subscriber sub_hall_dt8;
-    //ultra sonic range sensor
-    ros::Subscriber sub_usr;
-    ros::Subscriber sub_usl;
-    ros::Subscriber sub_usf;
-    ros::Subscriber m_sub_motor_speed;
+    ros::Subscriber sub_hall_counter; //subscriber for hall sensor counter
+    ros::Subscriber sub_hall_dt;      //subscriber for hall sensor, time between one tick
+    ros::Subscriber sub_hall_dt8;     //subscriber for hall sensor, time between eight ticks
+    ros::Subscriber sub_usr;          //subscriber for ultra sonic sensor right
+    ros::Subscriber sub_motor_speed_; //subscriber for set_motor_level_msg
 
  //Advertiser
-  //ros::Publisher pub_usr;
-    ros::Publisher m_pub_motor_speed;
-    ros::Publisher m_pub_steering;
-
-    ros::Publisher m_pub_velocity;
-    ros::Publisher m_pub_avg_speed;
-    ros::Publisher m_pub_distance;
-    ros::Publisher pub_usr;
-    ros::Publisher m_pub_wheel_odom;
+    ros::Publisher pub_avg_speed;     //publishes average speed every 5 seconds
+    ros::Publisher pub_distance;      //publishes driven distance of the car since last start
+    ros::Publisher pub_usr;           //publishes filtered data of ultra sonic sensor right
+    ros::Publisher pub_wheel_odom;    //publishes velocity of the car in TwistWithCovariance messag
   // Functions
-    //hall sensor
+
+    //----------------hall sensor----------------
+
+    /* NAME: hall_cnt_Callback
+     * DESCRIPTION: copies hall_cnt_msg into workspace variable
+     * INPUT: std_msgs::UInt8::ConstPtr hall_cnt_msg: constant Pointer on received message from subscriber
+     *        std_msgs::UInt8* m_hall_cnt: workspace variable, where the message is copied to
+     * OUTPUT: -
+     */
     static void hall_cnt_Callback(std_msgs::UInt8::ConstPtr hall_cnt_msg, std_msgs::UInt8* m_hall_cnt);
+    /* NAME: hall_dt_Callback
+     * DESCRIPTION: copies hall_dt_msg into workspace variable
+     * INPUT: std_msgs::Float64::ConstPtr hall_dt_msg: constant Pointer on received message from subscriber
+     *        std_msgs::Float64* msg_hall_dt: workspace variable, where the message is copied to
+     * OUTPUT: -
+     */
     static void hall_dt_Callback(std_msgs::Float64::ConstPtr hall_dt_msg, std_msgs::Float64* msg_hall_dt);
+    /* NAME: hall_dt8_Callback
+     * DESCRIPTION: copies hall_dt8_msg into workspace variable
+     * INPUT: std_msgs::Float64::ConstPtr hall_dt8_msg: constant Pointer on received message from subscriber
+     *        std_msgs::Float64* msg_hall_dt8: workspace variable, where the message is copied in
+     * OUTPUT: -
+     */
     static void hall_dt8_Callback(std_msgs::Float64::ConstPtr hall_dt8_msg, std_msgs::Float64* msg_hall_dt8);
-    //ultra sonic range sensor
+
+    //----------------ultra sonic range sensor----------------
+
+    /* NAME: usrCallback
+     * DESCRIPTION: filters all zeros from the incoming values, the forwards the values to a lowpass filter
+     *              then copies the output of the filter into workspace variable
+     * INPUT: sensor_msgs::Range::ConstPtr usrMsg: constant Pointer on received message from subscriber
+     *        sensor_msgs::Range* m_usr: workspace variable, where the message is copied in
+     *        float* tau: cutoff frequency for the lowpass filter
+     * OUTPUT: -
+     */
     static void usrCallback(sensor_msgs::Range::ConstPtr usrMsg, sensor_msgs::Range* m_usr, float* tau);
-    static void uslCallback(sensor_msgs::Range::ConstPtr uslMsg, sensor_msgs::Range* m_usl);
-    static void usfCallback(sensor_msgs::Range::ConstPtr usfMsg, sensor_msgs::Range* m_usf);
-    static void motor_speed_Callback(std_msgs::Int16::ConstPtr motor_speed_msg, std_msgs::Int16* msg_motor_speed);
-
+    /* NAME: motorSpeedCallback
+     * DESCRIPTION:  copies motor_speed_msg into workspace variable
+     * INPUT: std_msgs::Int16::ConstPtr motor_speed_msg: constant Pointer on received message from subscriber
+     *        std_msgs::Int16* msg_motor_speed: workspace variable, where the message is copied in
+     * OUTPUT: -
+     */
+    static void motorSpeedCallback(std_msgs::Int16::ConstPtr motor_speed_msg, std_msgs::Int16* msg_motor_speed);
+    /* NAME: lowpass
+     * DESCRIPTION: lowpass filter of first order
+     * INPUT: float input: new value, which should be filtered
+     *        float output_old: last value, which was calculated by the lowpass filter
+     *        float tau: cutoff frequency of the lowpass filter
+     * OUTPUT: float: filtered result of input value
+     */
     static float lowpass(float input, float output_old, float tau);
-    void param_callback(pses_control::sensorConfig &config, uint32_t level);
-
-
-
+    /* NAME: paramCallback
+     * DESCRIPTION:
+     * INPUT: -
+     * OUTPUT: -
+     */
+    void paramCallback(pses_control::sensorConfig &config, uint32_t level);
 
 };
 
--- src/pses_sensor_node.cpp
+++ src/pses_sensor_node.cpp
@@ -1,31 +1,22 @@
 #include "pses_control/pses_sensor_node.hpp"
-#define _USE_MATH_DEFINES
-
-
 
 pses_sensor::pses_sensor() {          //Constructor
     // subscribers of the hall sensor
     sub_hall_counter= nh.subscribe<std_msgs::UInt8>("/uc_bridge/hall_cnt", 10, boost::bind(hall_cnt_Callback, _1, &msg_hall_counter));
     sub_hall_dt     = nh.subscribe<std_msgs::Float64>("/uc_bridge/hall_dt", 10, boost::bind(hall_dt_Callback, _1, &msg_hall_dt));
     sub_hall_dt8    = nh.subscribe<std_msgs::Float64>("/uc_bridge/hall_dt8", 10, boost::bind(hall_dt8_Callback, _1, &msg_hall_dt8));
-    m_sub_motor_speed = nh.subscribe<std_msgs::Int16>("/uc_bridge/set_motor_level_msg", 1, boost::bind(motor_speed_Callback, _1, &msg_motor_speed));
+    sub_motor_speed_ = nh.subscribe<std_msgs::Int16>("/uc_bridge/set_motor_level_msg", 1, boost::bind(motorSpeedCallback, _1, &msg_motor_speed));
     // subscribers for ultra sonic range sensors
-    sub_usr = nh.subscribe<sensor_msgs::Range>("/uc_bridge/usr", 10, boost::bind(usrCallback, _1, &m_usr, &tau));
-    sub_usl = nh.subscribe<sensor_msgs::Range>("/uc_bridge/usl", 10, boost::bind(uslCallback, _1, &m_usl));
-    sub_usf = nh.subscribe<sensor_msgs::Range>("/uc_bridge/usf", 10, boost::bind(usfCallback, _1, &m_usf));
+    sub_usr = nh.subscribe<sensor_msgs::Range>("/uc_bridge/usr", 10, boost::bind(usrCallback, _1, &m_usr, &usr_tau));
 
     pub_usr = nh.advertise<sensor_msgs::Range>("/sensornode/usr",10);
-    //m_pub_motor_speed = nh.advertise<std_msgs::Int16>("/uc_bridge/set_motor_level_msg", 1);
-    //m_pub_steering = nh.advertise<std_msgs::Int16>("/uc_bridge/set_steering_level_msg", 1);
-
     //publisher of velocity data
-    m_pub_velocity = nh. advertise<std_msgs::Float64>("/sensornode/velocity",10);
-    m_pub_distance = nh.advertise<std_msgs::Float64>("/sensornode/distance",10);
-    m_pub_avg_speed = nh.advertise<std_msgs::Float64>("/sensornode/avg_speed",10);
-    m_pub_wheel_odom = nh.advertise<geometry_msgs::TwistWithCovarianceStamped>("/sensornode/wheel_odom",10);
+    pub_distance = nh.advertise<std_msgs::Float64>("/sensornode/distance",10);
+    pub_avg_speed = nh.advertise<std_msgs::Float64>("/sensornode/avg_speed",10);
+    pub_wheel_odom = nh.advertise<geometry_msgs::TwistWithCovarianceStamped>("/sensornode/wheel_odom",10);
     // reconfigure server
     dynamic_reconfigure::Server<pses_control::sensorConfig>::CallbackType f;
-    f = boost::bind(&pses_sensor::param_callback, this, _1, _2);
+    f = boost::bind(&pses_sensor::paramCallback, this, _1, _2);
     server.setCallback(f);
 
     //set default value to variables
@@ -34,7 +25,6 @@ pses_sensor::pses_sensor() {          //Constructor
 }
 
 //Callback function for Hall_Counter
-//writes msg from Hall Coutner Topic to local Variable
 void pses_sensor::hall_cnt_Callback(std_msgs::UInt8::ConstPtr hall_cnt_msg, std_msgs::UInt8* m_hall_cnt){
     *m_hall_cnt = *hall_cnt_msg;
 }
@@ -45,34 +35,22 @@ void pses_sensor::hall_dt8_Callback(std_msgs::Float64::ConstPtr hall_dt8_msg, st
     *msg_hall_dt8 = *hall_dt8_msg;
 }
 
-//Callback functions for ultra sonic sensor
-void pses_sensor::usrCallback(sensor_msgs::Range::ConstPtr usrMsg, sensor_msgs::Range* m_usr, float* tau){
-    if(usrMsg->range != 0){
+//Callback function for ultra sonic sensor
+void pses_sensor::usrCallback(sensor_msgs::Range::ConstPtr usrMsg, sensor_msgs::Range* m_usr, float* usr_tau){
+    if(usrMsg->range != 0){                        //filters invalid zero values
        float output_old = m_usr->range;
        *m_usr = *usrMsg;
-       m_usr->range = pses_sensor::lowpass(usrMsg->range, output_old, *tau);
+       m_usr->range = pses_sensor::lowpass(usrMsg->range, output_old, *usr_tau);
     }
-}
-void pses_sensor::uslCallback(sensor_msgs::Range::ConstPtr uslMsg, sensor_msgs::Range* m_usl){
-    if(uslMsg->range !=0){
-        *m_usl = *uslMsg;
-    }
-}
-void pses_sensor::usfCallback(sensor_msgs::Range::ConstPtr usfMsg, sensor_msgs::Range* m_usf){
-        if(usfMsg->range != 0){
-            *m_usf = *usfMsg;
-        }
 }
 
-void pses_sensor::motor_speed_Callback(std_msgs::Int16::ConstPtr motor_speed_msg, std_msgs::Int16* msg_motor_speed){
+void pses_sensor::motorSpeedCallback(std_msgs::Int16::ConstPtr motor_speed_msg, std_msgs::Int16* msg_motor_speed){
     *msg_motor_speed = *motor_speed_msg;
 }
-//Callback function for dynamic reconfigure
-void pses_sensor::param_callback(pses_control::sensorConfig &config, uint32_t level) {
-  ROS_INFO("Reconfigure Request:\n Velocity: %d \n Steering: %d \n tau: %f", config.velocity, config.steering, config.tau);
-  target_speed.data = config.velocity;
-  target_steering_angle.data = config.steering;
-  tau = config.tau;
+
+void pses_sensor::paramCallback(pses_control::sensorConfig &config, uint32_t level) {
+  ROS_INFO("Reconfigure Request:\n usr_tau: %f", config.tau);
+  usr_tau = config.tau;
 }
 
 //lowpass filter function
@@ -80,11 +58,18 @@ float pses_sensor::lowpass(float input, float output_old, float tau){
     return ((input+output_old*tau)/(tau+1));
 }
 
-//calculates the actual velocity and the average Velocity over 5 seconds
 void pses_sensor::calculateVelocity(){
-    if(msg_motor_speed.data !=0){
+    if(msg_motor_speed.data >0){  //direction of driving
         velocity.data = DRIVEN_DISTANCE_PER_TICK / msg_hall_dt.data;
+    }
+    else if(msg_motor_speed.data <0){
+            velocity.data = DRIVEN_DISTANCE_PER_TICK / msg_hall_dt.data * (-1);
+        }
+    else {
+        velocity.data = 0;
+    }
         driven_distance.data = DRIVEN_DISTANCE_PER_TICK * msg_hall_dt.data;
+        //calculate average speed over 5 seconds
         if(data_count ==0){
             avg_speed.data = velocity.data;
             data_count++;
@@ -95,40 +80,20 @@ void pses_sensor::calculateVelocity(){
         }
         else if(data_count = 500){
             avg_speed.data /= data_count;
-            m_pub_avg_speed.publish(avg_speed);
+            pub_avg_speed.publish(avg_speed);
             data_count =0;
         }
     }
-    else{
-        velocity.data = 0;
-        data_count =0;
-        avg_speed.data =0;
-    }
-}
 
-void pses_sensor::publish_sensor_data(){
+void pses_sensor::publishSensorData(){
    pub_usr.publish(m_usr);
-   m_pub_velocity.publish(velocity);
-   m_pub_distance.publish(driven_distance);
+   pub_distance.publish(driven_distance);
    wheel_odom.twist.twist.linear.x = velocity.data;
    wheel_odom.header.frame_id = "base_link";
    wheel_odom.header.stamp = ros::Time::now();
-   m_pub_wheel_odom.publish(wheel_odom);
+   pub_wheel_odom.publish(wheel_odom);
 }
 
-void pses_sensor::set_powertrain(){
-    //m_pub_motor_speed.publish(target_speed);
-   // m_pub_steering.publish(target_steering_angle);
-}
-
-//resets the car's actuators to default values in case of abort of the main loop
-void pses_sensor::reset(){
-    ROS_INFO("Reset Sensor Node");
-    target_speed.data=0;
-    target_steering_angle.data=0;
-    m_pub_motor_speed.publish(target_speed);
-    m_pub_steering.publish(target_steering_angle);
-}
 
 //signal handler to recognize Strg-C abort request of the programm
 void signalHandler(int sig) {
@@ -143,10 +108,8 @@ int main(int argc, char** argv) {
     signal(SIGINT, signalHandler);
     while (ros::ok()) {
         sensornode.calculateVelocity();
-        sensornode.publish_sensor_data();
-        sensornode.set_powertrain();
+        sensornode.publishSensorData();
         if (stop_request) {
-            sensornode.reset();
             ros::shutdown();
         }
         ros::spinOnce();
